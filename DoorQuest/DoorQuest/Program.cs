965fac86333c1cea296fac1817be6d166795204c
NoLifeDark <s2101049@edu.bc.fi>
2023-03-27 09:33:15 +0300

    Nuolitehtävän ensimmäinen osa tehty

---
+++ DoorQuest/DoorQuest/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+namespace EnumDemo
+{
+    class Ovihommeli
+    {
+        static bool oviLukossa = false;
+        static bool oviKiinni = false;
+        static bool loppu = false;
+        enum Ovi
+        {
+            Auki = 1,
+            Kiinni,
+            Lukossa,
+            Loppu
+        };
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ovi on " + Ovi.Auki + ". Mitä haluat tehdä?");
+            while (loppu == false)
+            {
+                string käsky = Console.ReadLine();
+                if (käsky == "Kiinni")
+                {
+                    Console.WriteLine("Ovi on " + Ovi.Kiinni + ". Mitä haluat tehdä?");
+                    Ovihommeli.oviKiinni = true;
+                    Ovihommeli.oviLukossa = false;
+                }
+                if (käsky == "Lukko")
+                {
+                    if (!Ovihommeli.oviKiinni)
+                    {
+                        Console.WriteLine("Sulje ovi ennen kuin lukitset oven!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Ovi on " + Ovi.Lukossa + ". Mitä haluat tehdä?");
+                        Ovihommeli.oviLukossa = true;
+                        Ovihommeli.oviKiinni = false;
+                    }
+                }
+                if (käsky == "Auki")
+                {
+                    if (oviLukossa == true)
+                    {
+                        Console.WriteLine("Poista lukko!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Ovi on " + Ovi.Auki + ". Mitä haluat tehdä?");
+                        Ovihommeli.oviKiinni = false;
+                    }
+                }
+                if (käsky == "Loppu")
+                {
+                    loppu = true;
+                }
+            }
+        }
+    }
+}
---
+++ Nuolitehtävä/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+Console.WriteLine("Minkälainen kärki (puu, teräs vai timantti)? ");
+string nuoliKärki = Console.ReadLine().ToLower();
+
+Console.WriteLine("Minkälaiset sulat (lehti, kanansulka vai kotkansulka)? ");
+
+string nuoliPera = Console.ReadLine().ToLower();
+
+Console.WriteLine("Kuinka pitkän kärjen haluat (60cm - 100cm)? ");
+float nuoliPituus = float.Parse(Console.ReadLine());
+
+Nuoli nuoli = new Nuoli(nuoliKärki, nuoliPera, nuoliPituus);
+Console.WriteLine("Nuolesi hinta on " + nuoli.PalautaHinta() + " kultaa");
+
+public class Nuoli
+{
+    string nuoliPera;
+    string nuoliKärki;
+    float nuoliPituus;
+    double nuoliHinta;
+
+    public Nuoli(string NUOLIPERA, string NUOLIKARKI, float NUOLIPITUUS)
+    {
+        nuoliPera = NUOLIPERA;
+        nuoliKärki = NUOLIKARKI;
+        nuoliPituus = NUOLIPITUUS;
+        if (nuoliPera == "puu")
+        {
+            nuoliHinta = 3;
+        }
+        if (nuoliPera == "teräs")
+        {
+            nuoliHinta += 5;
+        }
+        if (nuoliPera == "timantti")
+        {
+            nuoliHinta += 50;
+        }
+        if (nuoliKärki == "kanansulka")
+        {
+            nuoliHinta += 1;
+        }
+        if (nuoliKärki == "kotkansulka")
+        {
+            nuoliHinta += 5;
+        }
+        nuoliHinta = nuoliHinta + nuoliPituus * 0.05;
+        return;
+    }
+    public double PalautaHinta()
+    {
+        return nuoliHinta;
+    }
+}
+
+
+enum NuoliKärki
+{
+    puu = 3,
+    teräs = 5,
+    timantti = 5
+}
+
+enum NuoliPera
+{
+    lehti = 0,
+    kanansulka = 1,
+    kotkansulka = 2
+}
+
